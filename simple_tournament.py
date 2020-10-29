acb00cd73e2ca2ef9a245a9181fddfea233273fc
adam-webster <43613194+adam-webster@users.noreply.github.com>
2020-10-29 22:13:11 +0000

    Update simple_tournament.py

--- simple_tournament.py
+++ simple_tournament.py
@@ -1,6 +1,8 @@
 import time
 t_import_0 = time.perf_counter() #to measure performance
 import axelrod as axl
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pprint
 t_import_1 = time.perf_counter()
@@ -24,17 +26,19 @@ def main():
     fig, ax = plt.subplots()
     title = ax.set_title('Payoff')
     xlabel = ax.set_xlabel('Strategies')
-    plot.boxplot(ax=ax).show()
+    plot.boxplot(ax=ax)
 
     #other results visuals available
-    plot.payoff().show()
+    plot.payoff()
 
-    #or save all available to file
-    plot.save_all_plots()
 
+    #or save all available to file, doesn't seem to work though
+    #plot.save_all_plots()
+
+    #to measure performance, help us determine if we will need any HPC time
+    t1 = time.perf_counter()
+    print(f"Imports took {t_import_1-t_import_0:0.5f} seconds")
+    print(f"Program ran in {t1-t_import_1:0.5f} seconds")
+
+    plt.show() #to show all plots called
 main()
-
-#to measure performance, help us determine if we will need any HPC time
-t1 =time.perf_counter()
-print(f"Imports took {t_import_1-t_import_0:0.5f} seconds")
-print(f"Program ran in {t1-t_import_1:0.5f} seconds")
