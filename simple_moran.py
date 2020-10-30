38178fad1740eb60747c6b28f95f221bcb9d65f5
adam-webster <43613194+adam-webster@users.noreply.github.com>
2020-10-30 19:16:43 +0000

    Update simple_moran.py

--- simple_moran.py
+++ simple_moran.py
@@ -4,7 +4,7 @@ import axelrod as axl
 import random, pprint
 import matplotlib.pyplot as plt
 t_import_1 = time.perf_counter()
-
+print(f"Imports took {t_import_1-t_import_0:0.5f} seconds")
 
 def main():
     #random list of strategies chosen
@@ -12,19 +12,16 @@ def main():
                axl.TitForTat(), axl.Grudger(),
                axl.Alternator(), axl.AdaptorBrief(),
                axl.AdaptorLong(), axl.Random(),
-               axl.Random(), axl.Random()]
+               axl.Retaliate(), axl.Punisher()]
 
     mp = axl.MoranProcess(players, seed=1)
     populations = mp.play()
-    print(len(mp))
-    print(mp.winning_strategy_name)
-
+    print(f"There were {len(mp)} rounds in the simple Moran game")
+    print(f"{mp.winning_strategy_name} won the simple Moran game")
     mp.populations_plot()
-
 
     #to measure performance, help us determine if we will need any HPC time
     t1 = time.perf_counter()
-    print(f"Imports took {t_import_1-t_import_0:0.5f} seconds")
     print(f"Simple Moran program ran in {t1-t_import_1:0.5f} seconds")
 
     plt.show() #finally show all plots called
@@ -33,19 +30,27 @@ main()
 def mutation():
     t2 = time.perf_counter()
     #random list of strategies chosen
+    '''
     players = [axl.Cooperator(), axl.Defector(),
                axl.TitForTat(), axl.Grudger(),
                axl.Alternator(), axl.AdaptorBrief(),
                axl.AdaptorLong(), axl.Random(),
                axl.Appeaser(), axl.CooperatorHunter()]
+               '''
+    players = [axl.Cooperator(), axl.Defector(),
+               axl.TitForTat(), axl.Grudger(),
+               axl.Alternator(), axl.AdaptorBrief(),
+               axl.AdaptorLong(), axl.Random(),
+               axl.Retaliate(), axl.Punisher()]
 
 
-    mp = axl.MoranProcess(players, mutation_rate=0.1)
+    mp = axl.MoranProcess(players, mutation_rate=0.1, seed=10)
+    #this loop stops the game running indefinitely
     for _ in mp:
         if len(mp.population_distribution()) == 1:
             break
-    mp.population_distribution()
-    print(mp.winning_strategy_name)
+    mp.population_distribution() #run the game
+    print(f"{mp.winning_strategy_name} won the mutation enabled game")
     mp.populations_plot()
 
     t3 = time.perf_counter()
